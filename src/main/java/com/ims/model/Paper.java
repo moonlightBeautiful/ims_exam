ca1b69782b4530bdb5f656935ab16f3ad3ca6a52
高旭 <xy20539@163.com>
2020-04-26 14:49:30 +0800

    Initial commit

---
+++ src/main/java/com/ims/model/Paper.java
@@ -0,0 +1,50 @@
+package com.ims.model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name="t_paper")
+public class Paper {
+
+	private int id;
+	private String paperName;
+	private Date joinDate;
+	private Set<Question> questions=new HashSet<Question>();
+	
+	@Id
+	@GeneratedValue(generator="_native")
+	@GenericGenerator(name="_native",strategy="native")
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPaperName() {
+		return paperName;
+	}
+	public void setPaperName(String paperName) {
+		this.paperName = paperName;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	
+	@OneToMany(mappedBy="paper",fetch=FetchType.EAGER)
+	public Set<Question> getQuestions() {
+		return questions;
+	}
+	public void setQuestions(Set<Question> questions) {
+		this.questions = questions;
+	}
+	
+	
+}
